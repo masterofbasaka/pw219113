4e6ecdc6eacc709a52700298070ec580f81ff65b
masterofbasaka <enri123415@gmail.com>
2019-04-29 13:59:05 -0600

    conexionala base de datos

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const myConnection= require('express-myconnection');
 const app = express();
 
 
 const indiceRutas=require('./rutas/index');
 
+app.set('port',process.env.PORT || 3000);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
+//middleware
+app.use(myConnection(mysql,{
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'crudnodejsmysql13'
+},'single'));
 
 app.use("/",indiceRutas);
 
-app.listen(4000,() => {
+app.listen(app.get('port'),() => {
 	console.log("servidor escuchando puerto 3000");
 })
