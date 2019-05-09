5d0f5d6af1810f81c55d28a21823daad96379c58
masterofbasaka <enri123415@gmail.com>
2019-05-09 13:52:39 -0600

    codigo mejorado de vue

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -8,6 +8,30 @@ const app = new Vue({
 			{nombre:'uva',cantidad: 0},
 			{nombre:'peras',cantidad: 80},
 			{nombre:'fresas',cantidad: 110}
-		]
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods:{
+		agregarFruta(){
+			//console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad: 0
+			});
+			this.nuevaFruta= '';
+		}
+		
+		},
+		computed:{
+			sumarfrutas(){
+				this.total= 0 ;
+				for(frutas of this.frutas2){
+					this.total = this.total+frutas.cantidad;
+				}
+				return this.total;
+			}
+
 	}
 });
